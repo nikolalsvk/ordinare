6a056d9ea44850f58d873548ea4a5c2f19e40d8b
nikolalsvk <nikolaseap@gmail.com>
2017-07-28 17:44:41 +0000

    Add CLI functionality

--- lib/ordinare.rb
+++ lib/ordinare.rb
@@ -1,7 +1,27 @@
 require "ordinare/version"
+require "optparse"
 
 module Ordinare
-  def self.hi
+  module_function
+
+  def hi
     "Ciao, sono Ordinare"
+  end
+
+  def parse_args
+    OptionParser.new do |opts|
+      opts.banner = "Usage: ordinare inside your Rails project"
+
+      opts.on("-v", "--version", "Check gem version") do
+        puts Ordinare::VERSION
+      end
+
+      opts.on("-h", "--help", "Get help") do
+        puts opts
+      end
+    end.parse!
+  end
+
+  def sort
   end
 end
